dd3be5ff2aa9851cdc1f95dd38089df63b8cedac
Kanja Victor <victorkaguri77@gmail.com>
2020-11-16 22:41:46 +0300

    Updated Commit

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ document.addEventListener("readystatechange", (event) => {
 });
 
 const initApp = () => {
-    //Add Listeners
+    //Add Event Listeners
 
     //Procedural Functionality
     //(1)Load list object.
@@ -22,7 +22,37 @@ const initApp = () => {
 const refreshThePage = () => {
     clearListDisplay();
     //(2)Render the list
+    renderList();
     //(3)Clear item entry field
     //(4)Set focus on item entry field
 }
 
+const clearListDisplay = () => {
+    //Parent element for all the list elements
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+}
+
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while(child){
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+}
+
+const renderList = () => {
+    const list = toDoList.getList;
+    list.forEach((item) => {
+        buildListItem(item);
+    });
+}
+
+const buildListItem = () => {
+    const div = document.createElement("div")
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+}
